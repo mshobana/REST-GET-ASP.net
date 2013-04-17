172eccf5e402a9a4b119458081c984458bea4529
Shobana <shobanam@thoughtworks.com>
2013-04-17 10:03:50 +0530

    Cleaned up template controller

---
+++ Eb_Bill_Server/Controllers/UnitsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eb_Bill_Server.Controllers
+{
+    public class CalculateController : Controller
+    {
+        // GET: /Calculate/Details/5
+
+        public String Details(int id)
+        {
+            return "hai";
+        }
+
+    }
+}
